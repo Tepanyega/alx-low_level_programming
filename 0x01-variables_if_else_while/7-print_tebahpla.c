a042a0f3e0fa12446309a8eaab41cf5839ed6a66
Tepanyega <samuelmonepe@gmail.com>
2023-07-14 11:53:23 -0700

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - A progaram that prints all single digits
+ * Return: (0) (Success)
+ */
+int main(void)
+{
+        char low;
+
+        for (low = 'z'; low >= 'a'; low--)
+                putchar(low);
+        putchar('\n');
+        return (0);
+}
